038f655fac93e6e0172628e234f7f90a0ca44421
Yaniv Grama <ygrama@dropbox.com>
2022-02-04 14:23:57 +0200

    add more examples

--- example/src/main/kotlin/Usages.kt
+++ example/src/main/kotlin/Usages.kt
@@ -19,10 +19,42 @@ fun main() {
     val password: String = KInquirer.promptInputPassword(message = "Enter Your Password:", hint = "password")
     println("Password: $password")
 
+    // Input Password Masked
+    val passwordMasked: String = KInquirer.promptInputPassword(
+        message = "Enter Your Password:",
+        hint = "password",
+        mask = "🤫"
+    )
+    println("Password: $passwordMasked")
+
     // List
     val size: String =
         KInquirer.promptList(message = "What size do you need?", choices = listOf("Large", "Medium", "Small"))
     println("Size: $size")
+
+    // List View Options
+    val continent: String = KInquirer.promptList(
+        message = "Select a continent:",
+        choices = listOf(
+            "Asia",
+            "Africa",
+            "Europe",
+            "North America",
+            "South America",
+            "Australia",
+            "Antarctica",
+        ),
+        hint = "press Enter to pick",
+        pageSize = 3,
+        viewOptions = ListViewOptions(
+            questionMarkPrefix = "🌍",
+            cursor = " 😎 ",
+            nonCursor = "    ",
+        )
+
+
+    )
+    println("Continent: $continent")
 
     // Checkbox
     val toppings: List<String> = KInquirer.promptCheckbox(
@@ -34,4 +66,29 @@ fun main() {
         ),
     )
     println("Toppings: $toppings")
+
+    // Checkbox View Options
+    val colors: List<String> = KInquirer.promptCheckbox(
+        message = "Which colors do you prefer?",
+        choices = listOf(
+            "Red",
+            "Green",
+            "Blue",
+            "Yellow",
+            "Black",
+            "White",
+        ),
+        hint = "pick a color using spacebar",
+        maxNumOfSelection = 3,
+        minNumOfSelection = 2,
+        pageSize = 3,
+        viewOptions = CheckboxViewOptions(
+            questionMarkPrefix = "❓",
+            cursor = " 👉 ",
+            nonCursor = "    ",
+            checked = "✅ ",
+            unchecked = "○ ",
+        )
+    )
+    println("Colors: $colors")
 }
